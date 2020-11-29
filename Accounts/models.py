790da48e3ebe8655f6880ee387a0a51d7d86b09c
TarekGhajary <tarek940gh@gmail.com>
2020-11-29 17:38:27 +0200

    Done

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profiles(models.Model):
+    UserName     = models.ForeignKey(User, on_delete=models.CASCADE)
+    ID_User      = models.CharField(primary_key=True, max_length=12, unique=True)
+    FirstName    = models.CharField(max_length=32, null=True)
+    LastName     = models.CharField(max_length=32, null=True)
+    Birthdate    = models.DateField(null=True)
+    Qs_Shares    = models.IntegerField(default=0)
+    Questionnais = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.UserName.username
